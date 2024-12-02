afe23ce101af682a2830d032f9fa097fe4693bf3
mikster36 <45michaelo88@gmail.com>
2024-12-02 00:19:30 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	pruning/random.py

--- pruning/magnitude.py
+++ pruning/magnitude.py
@@ -1,5 +1,4 @@
 import os
-
 import torch
 import sys
 from tqdm import tqdm
@@ -21,7 +20,6 @@ def prune_magnitude(
     total = 0
     weights = []
     masks = {}
-
     for i in range(len(prune_ratios) - 1, 0, -1):
         prune_ratio = prune_ratios[i]
         mask_path = f'masks/magnitude/{prune_ratio}_masks.pth'
@@ -30,7 +28,6 @@ def prune_magnitude(
             print(f'Loaded masks from {mask_path}')
             masks[str(prune_ratio)] = mask
             prune_ratios.remove(prune_ratio)
-
     if len(prune_ratios) == 0:
         return model, masks
 
@@ -46,7 +43,6 @@ def prune_magnitude(
     all_weights = torch.cat([w.flatten() for w, _ in weights])
     for prune_ratio in prune_ratios:
         num_to_prune = int(prune_ratio * total)
-        to_prune = weights[:num_to_prune].copy()
 
         # Get the threshold value for pruning
         threshold, _ = torch.topk(all_weights, num_to_prune, largest=False)
@@ -54,21 +50,12 @@ def prune_magnitude(
         # Apply pruning by creating masks for each parameter
         mask = {}
         with torch.no_grad():
-            for _, name, idx in tqdm(to_prune, 'Pruning weights', file=sys.stdout):
+            for weight_magnitude, name in tqdm(weights, 'Pruning weights', file=sys.stdout):
+                m = (weight_magnitude >= threshold[-1]).float()
                 param = dict(model.named_parameters())[name]
-                param.view(-1)[idx] = 0.0
-                # Do not set requires_grad to False; keep it True for future gradient updates
+                param.data.mul_(m)
 
-                # Optionally create a mask
-                module = dict(model.named_modules()).get(name, None)
-                if module and not hasattr(module, 'weight_mask'):
-                    module.weight_mask = torch.ones_like(param)
-                if module:
-                    module.weight_mask.view(-1)[idx] = 0.0
-
-                if name not in mask:
-                    mask[name] = torch.ones_like(param)
-                mask[name].view(-1)[idx] = 0.0
+                mask[name] = m
 
         if not os.path.exists('./masks'):
             os.mkdir('./masks/')
